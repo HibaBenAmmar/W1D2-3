249c09f42da5ce32faed99c0bc016486fabb1398
HibaBenAmmar <86246905+HibaBenAmmar@users.noreply.github.com>
2021-07-06 09:16:40 +0200

    Add files via upload

---
+++ W1D2-3/script.js
@@ -0,0 +1,13 @@
+console.log("Hallo Winc Academy studenten");
+
+/*
+// Dit is een grote som.
+let sum = 1230941 * 1823794;
+console.log ("sum:", sum);
+
+let division = 8 / 2;
+console.log ("division:" , division);
+
+// Aantekeningen worden niet gelezen */
+
+//Je ziet geen uitkomt omdat we de code in een blok comment hebben gedaan.
